52980360eee7214d2d53fe99958b637839beee75
omar9112 <omar91@dhcp-crl-116-243.ece.uprm.edu>
2013-09-18 15:55:50 -0400

    No change

--- Project/appjs/product.js
+++ Project/appjs/product.js
@@ -1,7 +1,7 @@
 function Product(name, model, instantPrice, bidPrice, description){
 	this.id = "";
 	this.name = name;
-		this.model = model;
+	this.model = model;
 	this.bidPrice = bidPrice;
 	this.instantPrice = instantPrice;
 	this.description = description;

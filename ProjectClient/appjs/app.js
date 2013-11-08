0a15875453d513176edeaf13e1c1cb3b2cea2e07
omar9112 <omar91@Christians-MacBook-Pro-2.local>
2013-11-08 17:32:01 -0400

    some cleaning

--- ProjectClient/appjs/app.js
+++ ProjectClient/appjs/app.js
@@ -1,18 +1,3 @@
-// $(document).on( "pageinit", function() {
-    // $( document ).on( "swipeleft swiperight", function( e ) {
-        // // We check if there is no open panel on the page because otherwise
-        // // a swipe to close the left panel would also open the right panel (and v.v.).
-        // // We do this by checking the data that the framework stores on the page element (panel: open).
-        // if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
-            // if ( e.type === "swipeleft"  ) {
-                // $( "#rightPanel" ).panel( "open" );
-            // } else if ( e.type === "swiperight" ) {
-                // $( "#leftPanel" ).panel( "open" );
-            // }
-        // }
-    // });
-// });
-
 // Validate register page
 $(document).on("pageshow", "#registerPage", function() {
 	$("#registerForm").validate({
@@ -452,7 +437,6 @@ function GetProduct(id){
 	});
 }
 
-//TODO
 function GetProductByCategory(category){
 	$.mobile.loading("show");
 	console.log("testing");
